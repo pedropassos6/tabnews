e401aca4505f709a50325b1e212cd3f040c95b3e
Pedro Henrique Passos <56487862+pedropassos6@users.noreply.github.com>
2025-01-27 21:45:25 +0000

    add scripts lint:check and lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tabnews
+
 forum style website
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Teste</h1>
+function Home() {
+  return <h1>Teste</h1>;
+}
+
+function teste2() {
+  console.log("identacao errada");
 }
 
 export default Home;
